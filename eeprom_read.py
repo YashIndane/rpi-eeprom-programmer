cc7958d4a66ef56f8b624dbb66e3bf74a439e57a
YashIndane <yashindane46@gmail.com>
2023-11-15 01:44:52 +0530

    single read & write success

--- eeprom_read.py
+++ eeprom_read.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 import time
+import logging
+import argparse
 import RPi.GPIO as GPIO
 
+#$ sudo python3 eeprom_read.py --addr="<HEX-ADDR>"
 
 def hex_to_bin(hex_data:str) -> str:
     scale = 16
@@ -20,47 +23,62 @@ def bin_to_hex(bin_data:str) -> str:
 
 def read_data(address:str) -> str:
 
-    GPIO.output(WE, GPIO.HIGH)
     GPIO.output(OE, GPIO.LOW)
 
     eeprom_addr = hex_to_bin(address)
     set_address(eeprom_addr)
+
+    time.sleep(1)
+
     byte_str = ""
 
     for pin in data_pins:
       byte_str += "1" if GPIO.input(pin) == 1 else "0"
 
     hex_str = bin_to_hex(byte_str)
+
     GPIO.output(OE, GPIO.HIGH)
+
     return hex_str
 
 
 def set_address(eeprom_address:str) -> None:
     for index, bit in enumerate(eeprom_address):
         GPIO.output(address_pins[index], GPIO.HIGH if bit == "1" else GPIO.LOW)
-        #insert time delay here to see read operation
 
 
 if __name__ == "__main__":
 
-    address_pins = [14,15,25,8,7,16,20,21]
-    data_pins = [17,27,22,10,9,11,13,26]
+    #Setting logging configuration
+    logging.basicConfig(level=logging.NOTSET)
+
+    #Parsing args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--addr", required=True, type=str, help="8 bit address in HEX")
+    args = parser.parse_args()
+    address = args.addr
+
+    address_pins = [14, 15, 25, 8, 7, 16, 20, 21]
+    data_pins = [17, 27, 22, 10, 9, 11, 13, 26]
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
 
+    #Setting up address, data, WE & OE pins
     for pi in address_pins:
         GPIO.setup(pi, GPIO.OUT)
 
     for pi_ in data_pins:
         GPIO.setup(pi_, GPIO.IN, GPIO.PUD_DOWN)
 
-    WE = 19
-    OE = 12
+    WE = 19 #Write Enable
+    OE = 12 #Output Enable
 
-    for pins in [WE,OE]:
+    for pins in [WE, OE]:
         GPIO.setup(pins, GPIO.OUT, initial=GPIO.HIGH)
 
-    print(read_data("0x12"))
+    GPIO.output(WE, GPIO.HIGH)
+
+    logging.info(read_data(address))
 
     GPIO.cleanup()
--- eeprom_write.py
+++ eeprom_write.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 import time
+import argparse
+import logging
 import RPi.GPIO as GPIO
+
+#$ sudo python3 eeprom_write.py --addr="<HEX-ADDR>" --data="<HEX-DATA>"
 
 def hex_to_bin(hex_data:str) -> str:
     scale = 16
@@ -17,20 +21,23 @@ def bin_to_hex(bin_data:str) -> str:
     return hex_data
 
 
-def write(addr, data) -> None:
+def write(addr:str, data:str) -> None:
 
     set_address(hex_to_bin(addr))
     set_data(hex_to_bin(data))
+    time.sleep(1)
     
     GPIO.output(WE, GPIO.LOW)
     time.sleep(0.5)
     GPIO.output(WE, GPIO.HIGH)
 
+    logging.info("Write Successful!")
 
-    
+
 def set_address(eeprom_address:str) -> None:
     for index, bit in enumerate(eeprom_address):
         GPIO.output(address_pins[index], GPIO.HIGH if bit == "1" else GPIO.LOW)
+
 
 def set_data(eeprom_data:str) -> None:
     for index, bit in enumerate(eeprom_data):
@@ -39,15 +46,29 @@ def set_data(eeprom_data:str) -> None:
 
 if __name__ == "__main__":
 
-    address_pins = [14,15,25,8,7,16,20,21]
-    data_pins = [17,27,22,10,9,11,13,26]
+    #Setting logging configuration
+    logging.basicConfig(level=logging.NOTSET)
+    
+    #Parsing args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--addr", required=True, type=str, help="8 bit address in HEX")
+    parser.add_argument("--data", required=True, type=str, help="8 bit data in HEX")
+    args = parser.parse_args()
+    address = args.addr
+    data = args.data
 
-    WE = 19
-    OE = 12
+
+    address_pins = [14, 15, 25, 8, 7, 16, 20, 21]
+    data_pins = [17, 27, 22, 10, 9, 11, 13, 26]
+
+    WE = 19  #Write Enable
+    OE = 12  #Output Enable
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
+    
 
+    #Setting up address, data, OE & WE pins
     for pi in address_pins:
         GPIO.setup(pi, GPIO.OUT)
 
@@ -59,7 +80,7 @@ if __name__ == "__main__":
 
     GPIO.output(OE, GPIO.HIGH)
 
-    write("0x12", "0x77")
+    write(address, data)
 
     GPIO.cleanup()
 
